1db12ab9adeaf9bb225c89dc66157b0c506da598
ToshaETang <a0320506@gmail.com>
2023-12-10 14:29:06 +0800

    finish SYMTAB

--- main.c
+++ main.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* getSection(char* text, int i, char *section, int j){
-    if(i+1 >= strlen(text)){
+char* getSection(char* text, int textIndex, char *section, int sectionIndex){
+    if(textIndex+1 >= strlen(text)){
         section = "";
     }
-    else if(text[i] == '\t'){
+    else if(text[textIndex] == '\t'){
         section = "";
     }else{
-        while(text[i] != '\t' && text[i] != '\n'){
-            section[j] = text[i];
-            i++;
-            j++;
+        while(text[textIndex] != '\t' && text[textIndex] != '\n'){
+            section[sectionIndex] = text[textIndex];
+            textIndex++;
+            sectionIndex++;
         }
-        section[j] = '\0';
+        section[sectionIndex] = '\0';
     }
     return section;
 }
@@ -33,6 +33,21 @@ int isInOPTAB(char* OPCODE, FILE* OPTAB){
 }
 
 
+void printTAB(char* fileName){
+    FILE* TAB;
+    TAB = fopen(fileName,"r"); // read
+
+    printf("--------------------------\n");
+    char* tmp = malloc(100*sizeof(char));
+    while(fgets(tmp,100,TAB) != NULL){
+        printf("%s\n",tmp);
+    }
+    printf("--------------------------\n");
+
+    fclose(TAB);
+}
+
+
 //==============================================================
 
 int main()
@@ -45,26 +60,26 @@ int main()
 
     //open file
     FILE* inputFile;
-    inputFile = fopen ("inputFile.txt", "r"); // read
+    inputFile = fopen("inputFile.txt", "r"); // read
     FILE* OPTAB;
-    OPTAB = fopen ("OPTAB.txt", "r"); // read
+    OPTAB = fopen("OPTAB.txt", "r"); // read
     FILE* SYMTAB;
-    SYMTAB = fopen ("SYMTAB.txt", "w+"); //overwrite or create
+    SYMTAB = fopen("SYMTAB.txt", "w+"); //overwrite or create
     FILE* resultFile;
-    resultFile = fopen ("resultFile.txt", "w+"); //overwrite or create
+    resultFile = fopen("resultFile.txt", "w+"); //overwrite or create
     FILE* interMediateFile;
-    interMediateFile = fopen ("interMediateFile.txt", "w+"); //overwrite or create
+    interMediateFile = fopen("interMediateFile.txt", "w+"); //overwrite or create
 
 
     char* text = malloc(100*sizeof(char));
     
-
+    // 【start pass 1】
     while(fgets(text,100,inputFile) != NULL){
 
         //printf("%s\n",text);
-        char* LABEL = malloc(20*sizeof(char)); //eg. FIRST
-        char* OPCODE = malloc(20*sizeof(char)); //eg. STL
-        char* OPERAND = malloc(20*sizeof(char)); //eg. RETADR
+        char* LABEL = malloc(20*sizeof(char));
+        char* OPCODE = malloc(20*sizeof(char));
+        char* OPERAND = malloc(20*sizeof(char));
 
         //seperate the input line
         LABEL = getSection(text, 0, LABEL, 0);
@@ -74,52 +89,70 @@ int main()
         //printf("OPCODE =%s|  ",OPCODE);
         //printf("OPERAND = %s\n",OPERAND);
 
-        //-------------
+        //-------------make SYMTAB--------------------------
 
         // OPCODE is START
         if(strcmp(OPCODE, "START")==0){
             STARTADDRESS = atoi(OPERAND); // str->int
             LOCCTER = STARTADDRESS;
             continue;
-        }else{
-            LOCCTER = 0;
         }
 
 
-        //-------------
         // OPCODE is not END
         if(strcmp(OPCODE, "END") != 0){
+
+            // save LABEL's LOCCTER 
             if(strcmp(LABEL, "") != 0){
                 char sLOCCTER[20];
-                itoa(LOCCTER, sLOCCTER, 16); // 10->16  int->str
+                itoa(LOCCTER, sLOCCTER, 16); // 10->16, int->str
                 fprintf(SYMTAB, "%s %s\n", LABEL, sLOCCTER);
             }
             
-            //search OPTAB
+            //search OPCODE in OPTAB
             if(isInOPTAB(OPCODE,OPTAB) == 1){ //IN OPTAB
                 //printf("%s IS IN OPTAB\n",OPCODE);
-
+                if(OPCODE[0] == '+'){
+                    LOCCTER = LOCCTER + 4; // format 4
+                }else if(OPCODE[strlen(OPCODE)-1] == 'R'){
+                    LOCCTER = LOCCTER + 2; // format 2
+                }else{
+                    LOCCTER = LOCCTER + 3; // format 3
+                }
             }else{ //NOT IN OPTAB
                 //printf("%s IS NOT IN OPTAB\n",OPCODE);
+                if(strcmp(OPCODE, "WORD")==0){
+                    LOCCTER = LOCCTER + 3;
+                }else if(strcmp(OPCODE, "RESW")==0){
+                    LOCCTER = LOCCTER + (atoi(OPERAND)*3);
+                }else if(strcmp(OPCODE, "RESB")==0){
+                    LOCCTER = LOCCTER + atoi(OPERAND);
+                }else if(strcmp(OPCODE, "BYTE")==0){
+                    if(OPERAND[0]=='X'){
+                        LOCCTER = LOCCTER + 1;
+                    }else{
+                        LOCCTER = LOCCTER + (strlen(OPERAND)-3); // C''
+                    }
+                }
             }
-            rewind(OPTAB);
-            //strcmp(OPCODE, "RESW")==0 || strcmp(OPCODE, "RESB")==0 || strcmp(OPCODE, "BYTE")==0
             
-        }//if(strcmp(OPCODE, "END") != 0)
+            rewind(OPTAB);  // reread file
+
+        }// OPCODE is not END
+
+    }// 【end pass 1】
+
+    fclose(SYMTAB); // save file
+
+    printTAB("SYMTAB.txt");
+
+    //-------------finish SYMTAB--------------------------
 
 
 
-
-
-    }//while(fgets(text,100,inputFile) != NULL)
-
-     
-
-
-    //----------------------------
+    //----------------------------------------------------
     fclose(inputFile);
     fclose(resultFile);
-    fclose(SYMTAB);
     fclose(OPTAB);
     fclose(interMediateFile);
     
