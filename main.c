c0e90baffcc134e76b35c1d36161ba2d1c46316f
ToshaETang <a0320506@gmail.com>
2023-12-25 12:26:50 +0800

    pass 2 (half)

--- main.c
+++ main.c
@@ -32,6 +32,7 @@ void changeInputFile(){
     fclose(inputFile);
 }
 
+
 char* getSection(char* text, int textIndex, char *section, int sectionIndex){
     if(textIndex+1 >= strlen(text)){
         section = "";
@@ -62,6 +63,7 @@ int isInOPTAB(char* OPCODE, FILE* OPTAB){
     }
     return 0;
 }
+
 
 int isInSYMTAB(char* OPERAND2, FILE* SYMTAB){
     char* text = malloc(100*sizeof(char));
@@ -94,6 +96,62 @@ void printTAB(char* fileName){
     fclose(TAB);
 }
 
+
+int getCompareAdresss(char* OPCODE){
+    FILE* OPTAB = fopen("OPTAB.txt","r");
+    char* text = malloc(100*sizeof(char));
+    
+    while(fgets(text,100,OPTAB) != NULL){
+        char* label = malloc(20*sizeof(char));
+        label = getSection(text, 0, label, 0);
+        if(strcmp(OPCODE, label) == 0){
+            char* comadd = malloc(20*sizeof(char));
+            int comaddress = strtol(getSection(text, strlen(label)+1, comadd, 0),NULL,16);
+            return comaddress;
+        }
+    }
+    fclose(OPTAB);
+    return 0;
+}
+
+int getCompareSymbolValue(char* OPERAND){
+    FILE* SYMTAB = fopen("SYMTAB.txt","r");
+    char* text = malloc(100*sizeof(char));
+    
+    while(fgets(text,100,SYMTAB) != NULL){
+        char* label = malloc(20*sizeof(char));
+        label = getSection(text, 0, label, 0);
+        //printf("%s\n",label);
+        if(strcmp(OPERAND, label) == 0){
+            char* comsym = malloc(20*sizeof(char));
+            int comsymbolvalue = strtol(getSection(text, strlen(label)+1, comsym, 0),NULL,16);
+            //printf("%x %d\n",comsymbol,comsymbol);
+            return comsymbolvalue;
+        }
+    }
+    fclose(SYMTAB);
+    return 0;
+}
+
+
+int getPC(char* OPCODE){
+    FILE* MIDFILE = fopen("MIDFILE.txt","r");
+    char* text = malloc(100*sizeof(char));
+    
+    while(fgets(text,100,MIDFILE) != NULL){
+        char* opcode = malloc(20*sizeof(char));
+        char* pc = malloc(20*sizeof(char));
+        pc = getSection(text, 0, pc, 0);
+        opcode = getSection(text, strlen(pc)+1, opcode, 0);
+        //printf("%s\n",label);
+        if(strcmp(OPCODE, opcode) == 0){
+            return strtol(pc,NULL,16);
+        }
+    }
+    fclose(MIDFILE);
+    return 0;
+
+}
 
 //==============================================================
 
@@ -218,12 +276,14 @@ int main()
     FILE* RESULTFILE;
     RESULTFILE = fopen("resultFile.txt", "w+"); //overwrite or create
 
+    int N,I,X,B,P,E,format,invalidopcode;
+
     char* text2 = malloc(100*sizeof(char));
 
     while(fgets(text2,100,MIDFILE_2) != NULL){
 
-        int OPADDRESS;
-
+        int OPADDRESS,PC,textRecordLength=0;
+        char* mncode = malloc(20*sizeof(char));
         char* LOC = malloc(20*sizeof(char));
         char* LABEL2 = malloc(20*sizeof(char));
         char* OPCODE2 = malloc(20*sizeof(char));
@@ -233,25 +293,167 @@ int main()
         OPCODE2 = getSection(text2, strlen(LOC)+strlen(LABEL2)+2, OPCODE2, 0);
         OPERAND2 = getSection(text2, strlen(LOC)+strlen(LABEL2)+strlen(OPCODE2)+3, OPERAND2, 0);
 
-        printf("LOC = %s  |  ",LOC);
-        printf("LABEL2 = %s  |  ",LABEL2);
-        printf("OPCODE2 = %s|  ",OPCODE2);
-        printf("OPERAND2 = %s\n",OPERAND2);
+        //printf("LOC = %s  |  ",LOC);
+        //printf("LABEL2 = %s  |  ",LABEL2);
+        //printf("OPCODE2 = %s|  ",OPCODE2);
+        //printf("OPERAND2 = %s\n",OPERAND2);
 
         if(strcmp(OPCODE2, "START")==0){
             fprintf(RESULTFILE, "H%s\t%06x%06x\n", LABEL2, STARTADDRESS, PROGRAMLENGTH);
+            PC = STARTADDRESS;
         }
         else if(strcmp(OPCODE2, "END")!=0){ // NOT END
-
+            invalidopcode = 1;
+            PC = getPC(OPCODE2);
+            //printf("%s:%x\n",OPCODE2,PC);
             //search OPCODE in OPTAB
             if(isInOPTAB(OPCODE2,OPTAB) == 1){ //IN OPTAB
                 if(strcmp(OPERAND2, "")!=0){ // there is OPERAND
                     //printf("%s OPERAND ADDRESS = %d\n",OPERAND2, isInSYMTAB(OPERAND2, SYMTAB_2));
                     OPADDRESS = isInSYMTAB(OPERAND2, SYMTAB_2);
-                }
-            }else if(strcmp(OPCODE2, "BYTE")==0){ // do not need "WORD"
+                    //printf("%s : %d\n",OPERAND2,OPADDRESS);
+                    //printf("%s\n",OPCODE2);
+                    
+                    //assemble OPCODE2
+                    if(OPCODE2[strlen(OPCODE2)-1]=='R'){
+                        //printf("%s is FORMAT 2\n",OPCODE2);
+                        format = 2;
+                        N = 0;
+                        I = 0;
+                        X = 0;
+                        B = 0;
+                        P = 0;
+                        E = 0;
+                    }
+                    else{ // not format 2
+                        if(OPCODE2[0]=='+'){
+                            format = 4;
+                            E = 1;
+                        }
+                        else{
+                            E = 0;
+                            format = 3;
+                        }
 
+                        if(OPERAND2[0]=='#'){
+                            N=0;
+                            I=1;
+                            X=0;
+                            B=0;
+                            P=0;
+                            for(int i=0; OPERAND2[i]!='\0'; i++){
+                                OPERAND2[i]=OPERAND2[i+1];
+                            }
+                            //printf("  %s new:%s\n",OPCODE2,OPERAND2);
+                        }
+                        else if(OPERAND2[0]=='@'){
+                            N=1;
+                            I=0;
+                            X=0;
+                            B=0;
+                            P=0;
+                            for(int i=0; OPERAND2[i]!='\0'; i++){
+                                OPERAND2[i]=OPERAND2[i+1];
+                            }
+                        }
+                        else if(OPERAND2[strlen(OPERAND2)-1]=='X' && OPERAND2[strlen(OPERAND2)-2]==','){
+                            N=1;
+                            I=1;
+                            X=1;
+                            B=0;
+                            P=1;
+                            OPERAND2[strlen(OPERAND2)-2]='\0';
+                            //printf("  %s new:%s\n",OPCODE2,OPERAND2);
+                        }
+                        else{
+                            N=1;
+                            I=1;
+                            X=0;
+                            B=0;
+                            P=1;
+                        }
+                    }
+                }
+                // generate
+                //printf("%x\n",getCompareAdresss(OPCODE2));
+                //int compareAdresss = getCompareAdresss(OPCODE2);
+                invalidopcode = 0;
+                int compareAdresss = getCompareAdresss(OPCODE2);
+                int obj =  compareAdresss/16;
+                //printf("%x\n",obj);
+                char sobj[10];
+                itoa(obj,sobj,16);
+                strcat(mncode,sobj);
+                compareAdresss = compareAdresss%16;
+
+                if(N == 1) compareAdresss+=2;
+                if(I == 1) compareAdresss+=1;
+                itoa(compareAdresss,sobj,16);
+                strcat(mncode,sobj);
+                //printf("%s mncode:%s\n",OPCODE2,mncode);
+
+                if(format == 2){
+                    switch(OPERAND2[0])
+                    {
+                        case 'X' : strcat(mncode,"1");
+                                 break;
+                        case 'A' : strcat(mncode,"0");
+                                 break;
+                        case 'S' : strcat(mncode,"4");
+                                 break;
+                        case 'T' : strcat(mncode,"5");
+                                  break;
+                    }
+                    //printf("%s mncode:%s\n",OPCODE2,mncode);
+                }else{
+                    compareAdresss = 0;
+                    if(X==1)
+                        compareAdresss+=8;
+                    if(B==1)
+                        compareAdresss+=4;
+                    if(P==1)
+                        compareAdresss+=2;
+                    if(E==1)
+                        compareAdresss+=1;
+                    itoa(compareAdresss,sobj,16);
+                    strcat(mncode,sobj);
+                    //printf("%s mncode:%s\n",OPCODE2,mncode);
+                    if(E==1){
+                        if(N==1 && I==1){
+                            int compareSymbolValue = getCompareSymbolValue(OPERAND2);
+                            //printf("%s compareSymbol:%x\n",OPERAND2, compareSymbolValue);
+                            //strcat(mncode,"_0");
+                            itoa(compareAdresss,sobj,16);
+                            strcat(mncode,sobj);
+                        }
+                        //printf("%s mncode:%s\n",OPCODE2,mncode); !!!!!
+                    }
+                }
+
+                //printf("%s mncode:%s\n",OPCODE2,mncode);
+            }else if(strcmp(OPCODE2, "BYTE")==0){ // do not need "WORD"
+                format = 1;
+                if(OPERAND2[0]=='X'){
+                    for(int i =0 ; OPERAND2[i]!='\0';i++){
+                        OPERAND2[i]=OPERAND2[i+2];
+                    }
+                    OPERAND2[strlen(OPERAND2)-1]='\0';
+                    strcpy(mncode,OPERAND2);
+                    //printf("%s mncode:%s\n",OPCODE2,mncode);
+                }else{
+                    char ele[2];
+                    char buffer[20]="";
+                    for(int i = 2;i<strlen(OPERAND2)-1;i++){
+                        sprintf(ele,"%x",OPERAND2[i]);
+                        strcat(buffer,ele);
+                    }
+                    strcat(mncode,buffer);
+                    //printf("%s mncode:%s\n",OPCODE2,mncode);
+                }
             }
+            printf("%s objectCode:%s\n",OPCODE2,mncode);
+
+
             rewind(OPTAB);  // reread file
             rewind(SYMTAB);
 
@@ -263,7 +465,7 @@ int main()
 
     
 
-    //printf("\n  ===========END PASS 2=========== \n");
+    printf("\n  ===========END PASS 2=========== \n");
     // 【end pass 2】
     //----------------------------------------------------
 
